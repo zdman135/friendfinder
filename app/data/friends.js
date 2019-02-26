f6d36b3d8c046c08326000e55920bc54c3a43aa4
Rich Jen <rich.rjen@gmail.com>
2019-02-26 00:40:49 -0500

    removed bad profile links

--- app/data/friends.js
+++ app/data/friends.js
@@ -38,11 +38,7 @@ var friends = [{
 	"name": "anon",
 	"photo": "https://cdn.vox-cdn.com/thumbor/kskMuF6eE1PX09eZB4BNPZsQpD4=/0x0:2396x1597/1200x800/filters:focal(1210x441:1592x823)/cdn.vox-cdn.com/uploads/chorus_image/image/59627541/anon.0.jpg",
 	"scores": ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
-}, {
-	"name": "Ezra",
-	"photo": "https://www.google.com/imgres?imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D206567099358449&imgrefurl=https%3A%2F%2Fwww.facebook.com%2FTheIncrediblesFrozone%2F&docid=UKocQ1aRO9AgqM&tbnid=fB_NSnLTpKJmJM%3A&vet=10ahUKEwjB9LDiptLgAhUh4oMKHcM8Bu8QMwh0KBIwEg..i&w=800&h=800&bih=722&biw=1536&q=frozone&ved=0ahUKEwjB9LDiptLgAhUh4oMKHcM8Bu8QMwh0KBIwEg&iact=mrc&uact=8",
-	"scores": ["1", "5", "3", "4", "4", "2", "4", "1", "2", "1"]
-}, {
+},  {
 	"name": "Jim",
 	"photo": "https://i.imgflip.com/2ev98a.jpg",
 	"scores": ["2", "1", "4", "3", "1", "2", "4", "2", "3", "2"]
@@ -58,14 +54,6 @@ var friends = [{
 	"name": "Ashley",
 	"photo": "https://www.telegraph.co.uk/content/dam/news/2017/02/10/JS120174670_Berlins-polar-bear-cub-Fritz_NEWS_trans_NvBQzQNjv4Bqeo_i_u9APj8RuoebjoAHt0k9u7HhRJvuo-ZLenGRumA.jpg?imwidth=450",
 	"scores": ["5", "4", "3", "2", "2", "1", "3", "1", "5", "5"]
-}, {
-	"name": "Regine ",
-	"photo": "https://www.facebook.com/photo.php?fbid=10214094529581826&set=a.1575882401543&type=3&theater",
-	"scores": ["3", "1", "2", "2", "3", "4", "3", "4", "2", "3"]
-}, {
-	"name": "Monique ",
-	"photo": "https://www.facebook.com/photo.php?fbid=10207956337977557&set=a.1478365570368&type=3&theater",
-	"scores": ["2", "4", "5", "5", "2", "5", "5", "4", "3", "2"]
 }, {
 	"name": "mike",
 	"photo": "https://vignette.wikia.nocookie.net/green-productions-rise-of-sqeegee/images/5/54/Link.jpg/revision/latest?cb=20141223230033",
